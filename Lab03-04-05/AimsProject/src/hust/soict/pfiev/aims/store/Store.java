d6b4743721d2cc5fbfeebeecf0567d5f8fccc675
Tunn Tunn <onquangtung18052004@gmail.com>
2024-12-02 22:42:45 +0700

    lab04 2nd commit

--- Lab03-04-05/AimsProject/src/hust/soict/pfiev/aims/store/Store.java
+++ Lab03-04-05/AimsProject/src/hust/soict/pfiev/aims/store/Store.java
@@ -6,32 +6,30 @@ import java.util.List;
 import hust.soict.pfiev.aims.media.Media;
 
 public class Store {
-	
+
 	private List<Media> itemsInStore = new ArrayList<Media>();
-	
+
 	public void addMedia(Media media) {
-		if(itemsInStore.contains(media)) {
+		if (itemsInStore.contains(media)) {
 			System.out.println("The item is already in the Store!");
-		}
-		else {
+		} else {
 			itemsInStore.add(media);
 			System.out.println("Added successfully!");
 		}
 	}
-	
+
 	public void removeMedia(Media media) {
-		if(!itemsInStore.contains(media)) {
+		if (!itemsInStore.contains(media)) {
 			System.out.println("The item is not in the Store!");
-		}
-		else {
+		} else {
 			itemsInStore.remove(media);
 			System.out.println("Removed successfully!");
 		}
 	}
-	
+
 	public Media search(int id) {
-		for(Media media : itemsInStore) {
-			if(media.getId() == id) {
+		for (Media media : itemsInStore) {
+			if (media.getId() == id) {
 				System.out.println("Found");
 				return media;
 			}
@@ -39,10 +37,10 @@ public class Store {
 		System.out.println("Not found");
 		return null;
 	}
-	
+
 	public Media search(String title) {
-		for(Media media : itemsInStore) {
-			if(media.getTitle().equals(title)) {
+		for (Media media : itemsInStore) {
+			if (media.getTitle().equals(title)) {
 				System.out.println("Found");
 				return media;
 			}
@@ -50,13 +48,12 @@ public class Store {
 		System.out.println("Not found");
 		return null;
 	}
-	
+
 	public void print() {
 		System.out.println("***********************STORE***********************");
 		System.out.println("Store list:");
-		for(int i = 0; i < itemsInStore.size(); ++i) {
-			Media media = itemsInStore.get(i);
-			System.out.println((i + 1) + ". " + media);
+		for (Media m : this.itemsInStore) {
+			System.out.println(m.toString());
 		}
 		System.out.println("****************************************************");
 	}
